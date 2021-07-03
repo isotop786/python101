202a1249eccd3e32f919f3be66ad0ce7af7d1d0f
maruf786 <maruf@modello.my>
2021-07-03 17:29:47 +0600

    list

--- listSort.py
+++ listSort.py
@@ -1,1 +1,59 @@
-# Sorting list elements
+# # Sorting list elements
+
+# cars = ['bmw','audi','toyota','subaru']
+
+# # sorting permanently
+# cars.sort()
+
+# print('Permanently sorted list')
+# for c in cars: 
+#     print(c)
+
+# def print_job(item):
+#     print(f'Printing list __:')
+#     for i in item:
+#         print(i)
+
+# print_job(cars)        
+
+# new_cars = ['proton','chaga','manga','lamborgini','bugati','majda']
+
+# temp_sorted_car = sorted(new_cars)
+
+# print_job(temp_sorted_car)
+# print_job(new_cars)
+
+# printing list in revers order
+# listOfany = ['tv','laptop','smart phone','cell phone','telephone','tablet']
+
+# print(f'original list {listOfany}')
+# print('printing list in reverse order:')
+# # reverseList = listOfany.reverse()
+# # print_job(reverseList)
+# # print(reverseList)
+
+# listOfany.reverse()
+# print(f'reverse list: {listOfany}')
+
+list2 = ['any','star','music','game']
+
+print(f'Before reversing {list2}')
+
+list2.reverse()
+print(f'After reversing {list2}')
+
+# length of a list 
+# print(len(list2))
+print(f'length of {list2} is {len(list2)}')
+
+
+
+
+
+
+
+
+
+
+
+
